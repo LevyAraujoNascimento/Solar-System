587661676f3ce43fe77ffabfb6028ed3fe212dd1
Levy Araujo <levy4raujo@gmail.com>
2022-11-26 09:19:30 -0300

    5º e 6º requisito concluído

---
+++ src/components/PlanetCards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCards extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    const texto = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ texto } />
+      </div>
+    );
+  }
+}
+
+PlanetCards.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCards;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCards from './PlanetCards';
+import planets from '../data/planets';
+import { element } from 'prop-types';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetas = planets.map((element) => {
+      return (
+        <li key={ element.name }>
+          <PlanetCards planetName={ element.name } planetImage={ element.image } />
+        </li>
+      );
+    });
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          { planetas }
+        </ul>
       </div>
     );
   }

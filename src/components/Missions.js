bdd2341e6924742e3c5cee32b2989e39588d88e7
Levy Araujo <levy4raujo@gmail.com>
2022-11-26 10:26:51 -0300

    9º e 10º requisitos concluídos

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missoes = missions.map((missao) => {
+      const { name, year, country, destination } = missao;
+      return (
+        <li key={ name }>
+          <MissionCard name={ name } year={ year } country={ country } destination={ destination } />      
+        </li>  
+      );    
+    });
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>
+          { missoes }    
+        </ul>
       </div>
     );
   }
